9757112590879947b0d29d5c8145248d3fe5ef2c
Felix <86642452+FelixInOffice@users.noreply.github.com>
2021-12-13 11:48:12 +0800

    Add files via upload

---
+++ js/greenTree.js
@@ -0,0 +1,588 @@
+var trees = [{
+    x: 95,
+    y: 102,
+    id: 12,
+    loc: 'Tuen Mun EcoPark',
+    trs: 355
+},
+{
+    x: 220,
+    y: 31,
+    id: 49,
+    loc: 'Elegantia College',
+    trs: 66
+},
+{
+    x: 358,
+    y: 25,
+    id: 22,
+    loc: 'Baptist Assembly',
+    trs: 100
+},
+{
+    x: 189,
+    y: 42,
+    id: 9,
+    loc: 'The Kadoorie Institute - Shek Kong Centre (HKU)',
+    trs: 160
+},
+{
+    x: 95,
+    y: 79,
+    id: 24,
+    loc: 'Tuen Mun EcoPark (Phase 2)',
+    trs: 126
+},
+{
+    x: 408,
+    y: 31,
+    id: 52,
+    loc: 'Dai Wah Street & Dai Fuk Street',
+    trs: 201
+},
+{
+    x: 393,
+    y: 35,
+    id: 53,
+    loc: 'Tai Wo Road',
+    trs: 174
+},
+{
+    x: 377,
+    y: 44,
+    id: 35,
+    loc: 'Shatin Yuen Wo Road',
+    trs: 152
+},
+{
+    x: 384,
+    y: 104,
+    id: 44,
+    loc: 'City One Sha Tin ',
+    trs: 87
+},
+{
+    x: 368,
+    y: 65,
+    id: 32,
+    loc: 'Sha Tin Sheung Wo Che Road',
+    trs: 21
+},
+{
+    x: 361,
+    y: 48,
+    id: 54,
+    loc: 'Lam Tsuen Wishing Square',
+    trs: 99
+},
+{
+    x: 350,
+    y: 78,
+    id: 28,
+    loc: 'Sha Tin Man Lai Road',
+    trs: 7
+},
+{
+    x: 341,
+    y: 96,
+    id: 29,
+    loc: 'Tai Wai Shing Mun River Bank',
+    trs: 7
+},
+{
+    x: 360,
+    y: 109,
+    id: 27,
+    loc: 'Sha Tin Sha Tin Tau Road & Shing Tin Street',
+    trs: 42
+},
+{
+    x: 378,
+    y: 125,
+    id: 31,
+    loc: 'Sha Tin Sha Kok Street',
+    trs: 165
+},
+{
+    x: 251,
+    y: 225,
+    id: 15,
+    loc: 'Tai Kok Tsui Wai On Street',
+    trs: 45
+},
+{
+    x: 277,
+    y: 250,
+    id: 16,
+    loc: 'Tai Kok Tsui Fuk Tsun Street Chan Hing Social Service Centre',
+    trs: 27
+},
+{
+    x: 263,
+    y: 232,
+    id: 13,
+    loc: 'Tai Kok Tsui Sycamore Street & Sham Shui Po Maple Street',
+    trs: 155
+},
+{
+    x: 263,
+    y: 201,
+    id: 38,
+    loc: 'C.C.C. Heep Woh Primary School (Cheung Sha Wan)',
+    trs: 101
+},
+{
+    x: 278,
+    y: 208,
+    id: 42,
+    loc: 'Prince Edward Road West',
+    trs: 42
+},
+{
+    x: 277,
+    y: 232,
+    id: 8,
+    loc: 'Tai Kok Tsui Fuk Tsun Street (outside Kei Tsun Primary School)',
+    trs: 81
+},
+{
+    x: 251,
+    y: 190,
+    id: 57,
+    loc: 'Tack Ching Girls’ Secondary School',
+    trs: 113
+},
+{
+    x: 290,
+    y: 260,
+    id: 1,
+    loc: 'Pui Ching Primary School',
+    trs: 100
+},
+{
+    x: 289,
+    y: 220,
+    id: 41,
+    loc: 'C.C.C. Heep Woh Primary School (Prince Edward)',
+    trs: 51
+},
+{
+    x: 293,
+    y: 240,
+    id: 56,
+    loc: 'Maryknoll Fathers’ School (Primary Section)',
+    trs: 114
+},
+{
+    x: 307,
+    y: 242,
+    id: 36,
+    loc: 'Hong Kong College of Technology (Ho Man Tin Campus)',
+    trs: 48
+},
+{
+    x: 315,
+    y: 212,
+    id: 37,
+    loc: 'St. Francis of Assisi’s Caritas School',
+    trs: 98
+},
+{
+    x: 335,
+    y: 212,
+    id: 11,
+    loc: 'Pooi To Middle School',
+    trs: 110
+},
+{
+    x: 330,
+    y: 169,
+    id: 3,
+    loc: 'Lung Cheung Road (Near Wong Tai Sin Temple)',
+    trs: 260
+},
+{
+    x: 348,
+    y: 178,
+    id: 5,
+    loc: 'Lung Cheung Road (Near Wong Tai Sin Shopping Centre)',
+    trs: 125
+},
+{
+    x: 360,
+    y: 212,
+    id: 7,
+    loc: 'San Po Kong Po Leung Kuk Stanley Ho Sau Nan Primary School',
+    trs: 92
+},
+{
+    x: 345,
+    y: 230,
+    id: 34,
+    loc: 'Munsang College',
+    trs: 112
+},
+{
+    x: 370,
+    y: 241,
+    id: 17,
+    loc: 'HKU SPACE Community College (Kowloon East Campus)',
+    trs: 95
+},
+{
+    x: 374,
+    y: 261,
+    id: 51,
+    loc: 'Christian Family Service Centre Serene Oasis (Kwun Tong)',
+    trs: 318
+},
+{
+    x: 328,
+    y: 131,
+    id: 33,
+    loc: 'Tai Wai Hin Keng Street (Hin Keng Estate)',
+    trs: 30
+},
+{
+    x: 316,
+    y: 85,
+    id: 50,
+    loc: 'T.W.G.Hs Li Ka Shing College (Fanling)',
+    trs: 48
+},
+{
+    x: 388,
+    y: 277,
+    id: 59,
+    loc: 'St. Antonius Primary School',
+    trs: 134
+},
+{
+    x: 456,
+    y: 268,
+    id: 23,
+    loc: 'Lions – YMCA Junk Bay Youth Camp',
+    trs: 98
+},
+{
+    x: 488,
+    y: 286,
+    id: 6,
+    loc: 'Tseung Kwan O Yan Oi Tong Tin Ka Ping Primary School',
+    trs: 88
+},
+{
+    x: 381,
+    y: 354,
+    id: 43,
+    loc: 'HKUGA Primary School',
+    trs: 95
+},
+{
+    x: 359,
+    y: 330,
+    id: 58,
+    loc: 'Canossa School(Hong Kong) ',
+    trs: 69
+},
+{
+    x: 340,
+    y: 341,
+    id: 4,
+    loc: 'Marymount Primary School',
+    trs: 70
+},
+{
+    x: 324,
+    y: 329,
+    id: 25,
+    loc: 'Sheng Kung Hui Tang Shiu Kin Secondary School',
+    trs: 85
+},
+{
+    x: 309,
+    y: 335,
+    id: 2,
+    loc: 'St. Joseph Primary School',
+    trs: 39
+},
+{
+    x: 307,
+    y: 374,
+    id: 40,
+    loc: 'St. Paul’s Co-educational College Primary School',
+    trs: 86
+},
+{
+    x: 265,
+    y: 332,
+    id: 19,
+    loc: 'St. Stephen’s Girls’ Primary School',
+    trs: 75
+},
+{
+    x: 255,
+    y: 338,
+    id: 18,
+    loc: 'St. Stephen’s Girls’ College',
+    trs: 72
+},
+{
+    x: 271,
+    y: 367,
+    id: 21,
+    loc: 'The Aberdeen St. Peter’s Catholic Primary School',
+    trs: 68
+},
+{
+    x: 291,
+    y: 381,
+    id: 20,
+    loc: 'Victoria Shanghai Academy',
+    trs: 51
+},
+{
+    x: 185,
+    y: 185,
+    id: 55,
+    loc: 'TWGHs Chow Yin Sum Primary School',
+    trs: 98
+},
+{
+    x: 489,
+    y: 64,
+    id: 14,
+    loc: 'Chinese YMCA of Hong Kong Wu Kwai Sha Youth Village',
+    trs: 315
+},
+{
+    x: 473,
+    y: 69,
+    id: 39,
+    loc: 'Hong Kong College of Technology (Ma On Shan Campus)',
+    trs: 60
+},
+{
+    x: 459,
+    y: 83,
+    id: 26,
+    loc: 'Ma On Shan On Luk Street',
+    trs: 48
+},
+{
+    x: 426,
+    y: 97,
+    id: 30,
+    loc: 'Sha Tin Siu Lek Yuen Road',
+    trs: 221
+},
+{
+    x: 416,
+    y: 75,
+    id: 48,
+    loc: 'Baptist Sha Tin Wai Lui Ming Choi Primary School',
+    trs: 111
+},
+{
+    x: 408,
+    y: 110,
+    id: 47,
+    loc: 'Sha Tin Hang Shing Street to Lok Shing Street',
+    trs: 89
+},
+{
+    x: 398,
+    y: 85,
+    id: 46,
+    loc: 'Sha Tin Tak Wing Street',
+    trs: 171
+},
+{
+    x: 391,
+    y: 65,
+    id: 45,
+    loc: 'Sha Tin Pak Lee Street',
+    trs: 30
+},
+{
+    x: 428,
+    y: 26,
+    id: 10,
+    loc: 'The Hong Kong Institute of Education',
+    trs: 102
+},
+{
+    x: 405,
+    y: 72,
+    id: 60,
+    loc: 'Christ College',
+    trs: 60
+},
+{
+    x: 155,
+    y: 30,
+    id: 61,
+    loc: 'T.W.G.H.S. Yiu Dak Chi Memorial Primary School',
+    trs: 104
+},
+{
+    x: 108,
+    y: 124,
+    id: 63,
+    loc: 'Wu Chui Road (near Pierhead Garden)',
+    trs: 90
+},
+{
+    x: 120,
+    y: 125,
+    id: 62,
+    loc: 'Wu Chui Road (near Marina Garden)',
+    trs: 42
+},
+{
+    x: 143,
+    y: 115,
+    id: 64,
+    loc: 'Hang Fu Street (near Goodview Garden MTR Light Rail Station)',
+    trs: 96
+},
+{
+    x: 153,
+    y: 120,
+    id: 65,
+    loc: 'Hoi Wing Road',
+    trs: 75
+},
+{
+    x: 135,
+    y: 98,
+    id: 66,
+    loc: 'Ho Pong Street (near Tuen Mun MTR Station)',
+    trs: 39
+},
+{
+    x: 112,
+    y: 98,
+    id: 67,
+    loc: 'Ming Kum Road (near Kwong Kin Trade Centre)',
+    trs: 78
+},
+{
+    x: 125,
+    y: 85,
+    id: 68,
+    loc: 'Ming Kum Road (near Kin Fat Industrial Building)',
+    trs: 4
+},
+{
+    x: 127,
+    y: 107,
+    id: 69,
+    loc: 'Yip Wong Road (near Nan Fung Industrial City)',
+    trs: 105
+},
+{
+    x: 140,
+    y: 70,
+    id: 70,
+    loc: 'Tuen Fu Road',
+    trs: 33
+},
+{
+    x: 137,
+    y: 135,
+    id: 71,
+    loc: 'Yuet Wu Villa (near Siu Hei MTR Light Rail Station)',
+    trs: 216
+},
+{
+    x: 122,
+    y: 60,
+    id: 72,
+    loc: 'Hing Kwai Street (near Po Tin Estate Bus Terminus)',
+    trs: 45
+},
+{
+    x: 152,
+    y: 138,
+    id: 73,
+    loc: 'Tsing Sin Street (near Tsing Sin Street Basketball Court)',
+    trs: 64
+},
+{
+    x: 148,
+    y: 85,
+    id: 74,
+    loc: 'Shek Pai Tau Road central divider (near Greenland Garden)',
+    trs: 162
+}];
+
+/* create tree */
+for (var i = 0; i < trees.length; i++) {
+    var x = (trees[i].x - 88) * 1.8;
+    var y = (trees[i].y - 20) * 1.8;
+
+
+    var d = document.createElement('div');
+    let img = document.createElement("img");
+    img.setAttribute("src", "./img/tree.svg");
+    d.id = 'treed' + i;
+    d.className += 'htmlMapTree';
+    d.style.marginLeft = x + 'px';
+    d.style.marginTop = y + 'px';
+    d.appendChild(img);
+    document.getElementById('treeHere').appendChild(d);
+    $('#' + d.id).attr('data', trees[i].id);
+    $('#' + d.id).attr('curr', i);
+
+    //     /*
+    //      * create tree shadow
+    //      */
+
+    //     var s = document.createElement('div');
+    //     s.id = 'shadowd' + i;
+    //     s.className += 'htmlMapTreeShadow';
+    //     s.style.marginLeft = (x - 13) + 'px';
+    //     s.style.marginTop = (y + 10) + 'px';
+    //     document.getElementById('htmlMapBase').appendChild(s);
+
+    //     /*
+    //      * animation
+    //      */
+
+    //     var delay = 1000 + (randomId[i] / 4) * 85;
+
+    //     $('#' + d.id).transition({
+    //         opacity: 0
+    //     }, 0);
+    //     $('#' + d.id).transition({
+    //         opacity: 1,
+    //         delay: delay
+    //     }, 500);
+
+    //     // shadow
+    //     $('#' + s.id).transition({
+    //         opacity: 0
+    //     }, 0);
+    //     $('#' + s.id).transition({
+    //         opacity: 1,
+    //         delay: delay
+    //     }, 500);
+}
+
+
+/* darggable map */
+var $tree_container = $("#tree_container");
+
+function update() {
+    Draggable.create(".tree_box", {
+        bounds: $tree_container,
+        edgeResistance: 0.65,
+        type: "x,y",
+        throwProps: true,
+        autoScroll: true,
+    });
+}
+
+update();
+/* darggable map */
